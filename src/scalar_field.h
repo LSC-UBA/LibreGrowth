85bdfe4bcb46ed9dfd434c83ff81981c76a38faa
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2019-09-23 01:07:04 -0300

    Small modification in scalar_field.h

--- src/scalar_field.h
+++ src/scalar_field.h
@@ -29,19 +29,19 @@ public:
     ScalarField(const Mesh &mesh0);
 
     ScalarField(const Mesh &mesh0, long double val);
-
+    
     void init(const Mesh &mesh0, long double val);
-
+    
     void init(long double val);
 
     void set(const int i, const int j, const int k, long double val);
 
     long double get(const int i, const int j, const int k) const;
-
+    
     void save(const std::string &filename, const std::string &save_format);
-
+    
     const Mesh& get_mesh() const;
-
+    
     void print() const;
 
     long double get_norm() const;
@@ -49,7 +49,7 @@ public:
     // It assums that ii, jj and kk doesn't change
     ScalarField& operator =(const ScalarField & other);
 
-    long double & operator()(const int i, const int j, const int k) const;
+    long double& operator()(const int i, const int j, const int k) const;
 
     ScalarField operator +(const ScalarField & other) const;
     
